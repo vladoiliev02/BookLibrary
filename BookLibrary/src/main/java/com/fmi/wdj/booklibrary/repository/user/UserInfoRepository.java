d5b51ed64c446d8ec38d53cd7481a523cb8a39bb
vladoiliev02 <79850594+vladoiliev02@users.noreply.github.com>
2022-05-03 12:57:53 +0300

    Added security dependency.

--- BookLibrary/src/main/java/com/fmi/wdj/booklibrary/repository/user/UserInfoRepository.java
+++ BookLibrary/src/main/java/com/fmi/wdj/booklibrary/repository/user/UserInfoRepository.java
@@ -3,6 +3,6 @@ package com.fmi.wdj.booklibrary.repository.user;
 import com.fmi.wdj.booklibrary.model.user.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDetailsRepository extends JpaRepository<UserInfo, Long> {
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
 
 }
